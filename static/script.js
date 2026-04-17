6fd51f4dfcaf6e9c784e4f4e53ad9c1933260530
autunnomattia-crypto <autunno.mattia@iisgalvanimi.edu.it>
2026-04-17 07:46:42 +0000

    credenziali

---
+++ static/script.js
@@ -0,0 +1,9 @@
+async function controllacredenziali() {
+    const username = document.getElementById('username').value;
+    const pasword = document.getElementById('pasword').value;
+    if (!username || !pasword ) return alert("Scrivi un nome! e pasword");
+    const res = await fetch(`/login?username=${username}&pasword=${pasword}`);
+    const dati = await res.json();
+    document.getElementById('Risultato').innerText = dati.messaggio;
+}
+document.getElementById('btn-registrati').addEventListener('click',controllacredenziali)
