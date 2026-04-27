f9121abf07de6f25c72c07fcce3d5a05e391fe33
Fabrizio937 <corcio.fabrizio@iisgalvanimi.edu.it>
2026-04-27 09:12:32 +0000

    pandas

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+import pandas as pd
+
+df = pd.read_excel('dati.xlsx')
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -10,3 +13,19 @@ def home():
     # Restituisce direttamente il file HTML
     return FileResponse('static/index.html')
 
+@app.get("/login")
+def controlla(username: str, password: str):
+    print("username", username, "password", password)
+    if username == "admin" and password == "xxx123":
+        risposta = {"messaggio": 1}
+    else:
+        risposta = {"messaggio": 0}
+    return(risposta)
+
+@app.post("/pandas")
+def controlla_paszsword(username: str, password: str):
+    risultato = df[(df['USERNAME'] == username) & (df['PASSWORD'] == password)]
+    if not risultato.empty:
+        risposta = {"messaggio": 1}
+    else:
+        risposta = {"messaggio": 0}
--- static/script.js
+++ static/script.js
@@ -6,5 +6,45 @@ async function controllacredenziali() {
     const res = await fetch(`/login?username=${username}&password=${password}`);
     const dati = await res.json();
     document.getElementById('Risultato').innerText = dati.messaggio;
+    if (dati.messaggio === 1) {
+        document.getElementById('Risultato').innerText = "benvenuto nel sito";
+        document.getElementById('username').style.display = 'none';
+        document.getElementById('password').style.display = 'none';
+        document.getElementById('btn-registrati').style.display = 'none';
+    } else {
+        document.getElementById('Risultato').innerText = "password o utente sbagliato";
+        document.getElementById('username').value = "";
+        document.getElementById('password').value = "";
+    }
 }
 document.getElementById('btn-registrati').addEventListener('click',controllacredenziali)
+//---#aggiungere un bottone, controlla, e cliccando si manda le informazioni con i post---
+
+async function loggati_pandas() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    if (!username || !password )
+        return alert("Scrivi un nome e una password");
+    const res = await fetch(`/pandas`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    });
+    const dati = await res.json();
+    document.getElementById('Risultato').innerText = dati.messaggio;
+    if (dati.messaggio === 1) {
+        document.getElementById('Risultato').innerText = "benvenuto nel sito";
+        document.getElementById('username').style.display = 'none';
+        document.getElementById('password').style.display = 'none';
+        document.getElementById('btn-registrati').style.display = 'none';
+        document.getElementById('btn-pandas').style.display = 'none';
+    } else {
+        document.getElementById('Risultato').innerText = "password o utente sbagliato";
+        document.getElementById('username').value = "";
+        document.getElementById('password').value = "";
+    }
+}
+document.getElementById('btn-registrati').addEventListener('click',controllacredenziali)
+//---#aggiungere un bottone, controlla, e cliccando si manda le informazioni con i post---
